7ff3f1d7055c6e03cae8921ea2ebf5d682b1dc2c
Pasi Talvitie <pasi@superduper.eu>
2021-04-03 00:46:04 +0300

    Add create operation for indexes and mappings

--- src/elasticsearch.go
+++ src/elasticsearch.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
@@ -23,6 +24,82 @@ type ElasticsearchConfig struct {
 	ExportFilePath string
 }
 
+// ElasticCreateIndex creates an empty index to elasticsearch
+func ElasticCreateIndex(config *ElasticsearchConfig) {
+	// create new http client (TODO: Make timeout configurable)
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	indexURL := fmt.Sprintf("%s/%s", config.URL, config.IndexName)
+	req, err := http.NewRequest("PUT", indexURL, nil)
+	if err != nil {
+		if Debug {
+			DebugLogger.Println("Could form the request:", err)
+		}
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		if Debug {
+			DebugLogger.Println("Failed to create index:", err)
+		}
+		return
+	}
+	// Close response body
+	defer resp.Body.Close()
+	if strings.HasPrefix(resp.Status, "20") == false {
+		if Debug {
+			DebugLogger.Printf("Failed to create index (Status: %s)", resp.Status)
+			bodyByte, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				DebugLogger.Println(err)
+				return
+			}
+			DebugLogger.Println(string(bodyByte))
+		}
+	}
+}
+
+// ElasticCreateIndexWithMapping creates an empty index with mapping
+func ElasticCreateIndexWithMapping(config *ElasticsearchConfig, mapping *string) {
+
+	// create new http client (TODO: Make timeout configurable)
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	mappingEndpointURL := fmt.Sprintf("%s/%s/_mapping", config.URL, config.IndexName)
+	mappingBytes := []byte(*mapping)
+	req, err := http.NewRequest("PUT", mappingEndpointURL, bytes.NewBuffer([]byte(mappingBytes)))
+	if err != nil {
+		if Debug {
+			DebugLogger.Println("Could form the request:", err)
+		}
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		if Debug {
+			DebugLogger.Println("Failed to create mapping:", err)
+		}
+		return
+	}
+	// Close response body
+	defer resp.Body.Close()
+	if strings.HasPrefix(resp.Status, "20") == false {
+		if Debug {
+			DebugLogger.Printf("Failed to send data to Elasticsearch (Status: %s)", resp.Status)
+			bodyByte, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				DebugLogger.Println(err)
+				return
+			}
+			DebugLogger.Println(string(bodyByte))
+		}
+	}
+
+}
+
 // ElasticExporter exports data to either elasticsearch or file or both
 func ElasticExporter(wg *sync.WaitGroup, config *ElasticsearchConfig, resultSet []*Result) {
 
@@ -37,6 +114,7 @@ func ElasticExporter(wg *sync.WaitGroup, config *ElasticsearchConfig, resultSet 
 			}
 			return
 		}
+
 		requestData += fmt.Sprintf("{ \"index\" : { \"_index\" : \"%s\" } }\n", config.IndexName)
 		requestData += string(data)
 		requestData += "\n"
